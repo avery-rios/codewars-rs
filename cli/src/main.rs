00215484690dc18a735479a7f73d578b2278b70b
Avery Rios <avery-dd70@outlook.com>
2024-01-10 19:05:51 +0800

    [cli] add basic cli

---
+++ cli/src/main.rs
@@ -0,0 +1,144 @@
+extern crate codewars_api as api;
+extern crate codewars_solution as solution;
+
+use anyhow::{Context, Result};
+use api::Client;
+use clap::{FromArgMatches, Parser, Subcommand};
+use rustyline::Editor;
+use std::{fs, path::Path};
+use tokio::runtime::{self, Runtime};
+
+#[derive(Subcommand)]
+enum KataCmd {
+    /// Get kata information
+    Get { id: String },
+}
+async fn get_kata(id: &str, client: &Client, root: &Path) -> Result<()> {
+    use solution::*;
+    fn to_author(auth: api::Author) -> Author {
+        Author {
+            username: auth.username,
+            url: auth.url,
+        }
+    }
+    fn to_approve(
+        rank: Option<api::KataRank>,
+        approver: Option<api::Author>,
+        approved_at: Option<chrono::DateTime<chrono::Utc>>,
+    ) -> Option<KataApprove> {
+        Some(KataApprove {
+            rank: rank?.id,
+            approver: match approver {
+                Some(a) => to_author(a),
+                None => return None,
+            },
+            approved_at: approved_at?,
+        })
+    }
+
+    let kata = client
+        .get_challenge(id)
+        .await
+        .context("failed to get kata info")?;
+    let info = KataInfo {
+        name: kata.name,
+        id: kata.id,
+        slug: kata.slug,
+        url: kata.url,
+        approve: to_approve(kata.rank, kata.approved_by, kata.approved_at),
+        created_by: to_author(kata.created_by),
+        created_at: kata.published_at,
+        category: kata.category,
+        tags: kata.tags,
+    };
+    let kata_root = kata_path(root, &info);
+    if kata_root.exists() {
+        anyhow::bail!("Kata {} is already fetched", id);
+    }
+    fs::create_dir(&kata_root).context("failed to create kata dir")?;
+    println!("Kata {} will be saved to {}", id, kata_root.display());
+    write_kata(
+        kata_root,
+        &Metadata {
+            version: Version::CURRENT,
+            api_version: ApiVersion::CURRENT,
+            created_at: chrono::Local::now().fixed_offset(),
+            updated_at: Vec::new(),
+        },
+        &info,
+        &kata.description,
+    )
+    .context("failed to write kata")
+}
+impl KataCmd {
+    async fn run(self, client: &Client, root: &Path) -> Result<()> {
+        match self {
+            Self::Get { id } => get_kata(&id, client, root).await,
+        }
+    }
+}
+
+#[derive(Subcommand)]
+enum Command {
+    #[command(subcommand)]
+    Kata(KataCmd),
+    /// exit codewars cli
+    Exit,
+}
+impl Command {
+    async fn run(self, client: &Client, root: &Path) -> Result<bool> {
+        match self {
+            Self::Kata(k) => k.run(client, root).await?,
+            Self::Exit => return Ok(false),
+        }
+        Ok(true)
+    }
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+type LineEditor = Editor<(), rustyline::history::MemHistory>;
+
+fn read_exec(client: &Client, runtime: &Runtime, editor: &mut LineEditor) -> Result<bool> {
+    let inputs = shlex::split(&editor.readline("codewars> ")?).context("failed to split input")?;
+    let c = Command::augment_subcommands(clap::Command::new("repl"))
+        .multicall(true)
+        .try_get_matches_from(inputs)
+        .and_then(|m| Command::from_arg_matches(&m));
+    match c {
+        Ok(cmd) => runtime.block_on(cmd.run(client, Path::new("."))),
+        Err(e) => {
+            e.print().unwrap();
+            Ok(true)
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let runtime = runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .context("failed to create runtime")?;
+    let mut editor = Editor::with_history(
+        rustyline::Config::builder()
+            .auto_add_history(true)
+            .max_history_size(1000)
+            .unwrap()
+            .build(),
+        rustyline::history::MemHistory::new(),
+    )
+    .context("failed to create line editor")?;
+    let client = api::Client::new();
+    loop {
+        match read_exec(&client, &runtime, &mut editor) {
+            Ok(true) => (),
+            Ok(false) => break,
+            Err(e) => println!("error: {:?}", e),
+        }
+    }
+    Ok(())
+}
---
+++ solution/src/lib.rs
@@ -0,0 +1,87 @@
+use chrono::{DateTime, FixedOffset, Utc};
+use serde::{Deserialize, Serialize};
+use std::{
+    fs, io,
+    path::{Path, PathBuf},
+};
+
+pub use codewars_types::{rank, ApiVersion};
+use rank::KataRankId;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Author {
+    pub username: String,
+    pub url: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct KataApprove {
+    pub rank: KataRankId,
+    pub approver: Author,
+    pub approved_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct KataInfo {
+    pub name: String,
+    pub id: String,
+    pub slug: String,
+    pub url: String,
+    pub created_by: Author,
+    pub created_at: DateTime<Utc>,
+    pub approve: Option<KataApprove>,
+    pub category: String,
+    pub tags: Vec<String>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct Version(pub u8, pub u8);
+impl Version {
+    pub const CURRENT: Self = Self(0, 1);
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Metadata {
+    pub version: Version,
+    pub api_version: ApiVersion,
+    pub created_at: DateTime<FixedOffset>,
+    pub updated_at: Vec<DateTime<FixedOffset>>,
+}
+
+/// push kata path to a [PathBuf]
+/// Slug will be truncated if it's too long
+pub fn kata_path(root: impl AsRef<Path>, kata: &KataInfo) -> PathBuf {
+    const MAX_FILENAME: usize = 128;
+    let id_len = kata.id.len() + 2; // length of id and brackets
+    let slug = if kata.slug.len() + id_len > MAX_FILENAME {
+        &kata.slug[0..(MAX_FILENAME - id_len)]
+    } else {
+        kata.slug.as_str()
+    };
+    root.as_ref().join(format!("{}[{}]", slug, &kata.id))
+}
+
+/// save kata info under directory `root`
+/// - metadata file meta.json
+/// - kata info file info.json
+/// - kata description description.md
+/// - store solution under directory named by language,
+///   may be with a tag separated by dash. Like `haskell-tag1`
+pub fn write_kata(
+    mut root: PathBuf,
+    meta: &Metadata,
+    info: &KataInfo,
+    desc: &str,
+) -> io::Result<()> {
+    use serde_json::to_vec_pretty;
+    root.push("meta.json");
+    fs::write(&root, to_vec_pretty(meta).unwrap())?;
+    root.pop();
+
+    root.push("info.json");
+    fs::write(&root, to_vec_pretty(info).unwrap())?;
+    root.pop();
+
+    root.push("description.md");
+    fs::write(&root, desc)
+}
